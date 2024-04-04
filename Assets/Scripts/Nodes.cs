7c08be37e3a0083dbbb7b4fc9c43c8dd57a1e965
jorgegranadossobrino <jorge.granados.sobrino@alumnos.upm.es>
2024-04-04 10:32:14 +0200

    Programados casos de prueba JSON e interfaz básica

--- Assets/Scripts/Nodes.cs
+++ Assets/Scripts/Nodes.cs
@@ -1,24 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Nodes : MonoBehaviour
 {
-    [SerializeField] private GameObject[] nodes;
+    [SerializeField]
+    private GameObject[] nodes;
+    [SerializeField]
+    private List<GameObject> nodesPath;
+
+    private CircuitsList circuitsData = new CircuitsList();
     
     private Chrono chronoScript;
     private int currentNode = 0;
+    private TMP_Text testText;
+
+    [System.Serializable]
+    public class Circuit
+    {
+        public string name;
+        public TestCase[] test_case;
+    }
+    [System.Serializable]
+    public class TestCase
+    {
+        public string case_;
+        public int[] nodes;
+    }
+
+    [System.Serializable]
+    public class CircuitsList
+    {
+        public Circuit[] circuits;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        testText = GameObject.Find("TestCaseText").GetComponent<TMP_Text>();
         chronoScript = GameObject.Find("Controller").GetComponent<Chrono>();
+        TextAsset jsonFile = Resources.Load<TextAsset>("TestCases");
+
+
+        if (jsonFile != null)
+        {
+            circuitsData = JsonUtility.FromJson<CircuitsList>(jsonFile.text);
+            string activeSceneName = SceneManager.GetActiveScene().name;
+
+            Circuit circuit = circuitsData.circuits.FirstOrDefault(c => c.name == activeSceneName);
+
+            if (circuit != null)
+            {
+                int randomCase = (int)Random.Range(0,circuit.test_case.Length);
+
+                testText.text = circuit.test_case[randomCase].case_;
+
+                foreach (int nodeId in circuit.test_case[randomCase].nodes)
+                {
+                    GameObject nodeObject = nodes.FirstOrDefault(n => n.GetComponent<Node>().GetId() == nodeId);
+                    if (nodeObject != null)
+                    {
+                        nodesPath.Add(nodeObject);
+                    }
+                }
+            }           
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(currentNode == nodes.Length)
+        if(currentNode == nodesPath.Count)
         {
             if(!chronoScript.getFinishedRace())
             {
@@ -31,7 +87,7 @@ public class Nodes : MonoBehaviour
 
     public void CheckNode(int nodeToCheck)
     {
-        if(nodeToCheck == nodes[currentNode].GetComponent<Node>().GetId())
+        if(nodeToCheck == nodesPath[currentNode].GetComponent<Node>().GetId())
         {
             print(nodeToCheck);
             currentNode++;
